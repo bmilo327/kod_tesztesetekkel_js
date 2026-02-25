95203cc4ace22b3a32e94e858388c34175c4c7b4
= <=>
2026-02-25 15:11:48 +0100

    kesz teszt

--- feladatok.js
+++ feladatok.js
@@ -1,5 +1,14 @@
 export function egyedi_betuk(szoveg = "") {
   // ide kell írni a megoldást ciklussal és elágazással
- 
-  return [];
+  const lista = [];
+  for (let index = 0; index < szoveg.length; index++) {
+    let betu = szoveg[index].toLowerCase();
+    if (/^[a-záéíóöőúüű]$/.test(betu) && !lista.includes(betu)){
+      lista.push(betu);
+    }  
+    
+  }
+  lista.sort();
+  console.log(lista);
+  return lista;
 }
